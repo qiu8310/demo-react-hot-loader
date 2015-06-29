7478fc0c054c5cbf1557aff68431b4702d2fb02d
Mora <qiuzhongleiabc@126.com>
2015-06-29 23:21:33 +0800

    支持不同环境配置

--- scripts/App.jsx
+++ scripts/App.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+if (__DEBUG__) {
+  console.log('debug');
+}
+
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, x world.</h1>
+      <h1>Hello, world.</h1>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,32 @@
 var path = require('path');
 var webpack = require('webpack');
 
+var ENV = (process.env.NODE_ENV || 'development').toLowerCase();
+var PRODUCTION = /^prod/i.test(ENV);
+
+var appScripts = ['./scripts/index'];
+var plugins = [
+  // `webpack -p` 会将这些无法执行到的代码删除 ( dead-code elimination )
+  // FIXME 测试好像代码没有被删除
+  new webpack.DefinePlugin({ __DEBUG__: JSON.stringify(PRODUCTION ? false : true)}),
+  new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js')
+];
+
+if (!PRODUCTION) {
+  appScripts.unshift(
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server'
+  );
+  plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  );
+}
+
 module.exports = {
   devtool: 'eval',
   entry: {
-    app: [
-      'webpack-dev-server/client?http://localhost:3000',
-      'webpack/hot/only-dev-server',
-      './scripts/index'
-    ],
+    app: appScripts,
     vendor: ['react']
   },
   output: {
@@ -16,18 +34,14 @@ module.exports = {
     filename: 'app.js',
     publicPath: '/static/'
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
+  plugins: plugins,
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      loaders: ['react-hot', 'babel'],
+      loaders: (PRODUCTION ? [] : ['react-hot']).concat('babel'),
       include: path.join(__dirname, 'scripts')
     }]
   }
